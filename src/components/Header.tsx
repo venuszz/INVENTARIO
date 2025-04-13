edda17d8aa2fb77c5f153457b8aa7137d541947f
Antony110205 <tonyzape110205@gmail.com>
2025-04-13 12:57:48 -0600

    inventario-0.0.2.1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 export default function Header() {
     return (
-        <h1 className="text-red-500">Hola, este es el header</h1>
+        <h1 className="text-red-500">Hola, este es el header, y este es un cambio</h1>
     )
 }
